b9d2631fdacde28b16a71eedbc163476059e4bd8
InigoRomero <igoromero@gmail.com>
2021-02-04 22:50:23 +0100

    Feat: Avatars

--- src/reactChatBot.js
+++ src/reactChatBot.js
@@ -78,17 +78,17 @@ export default class reactChatBot extends Component {
     let userDiv = document.createElement("div");
     userDiv.id = "user";
     userDiv.className = "user response";
-    userDiv.innerHTML = `<img src="./user.png" class="avatar"><span>${this.state.text}</span>`;
+    userDiv.innerHTML = `<img src="`+ this.props.userIcon +`" class="avatar"><span>${this.state.text}</span>`;
     messagesContainer.appendChild(userDiv);
 
     let botDiv = document.createElement("div");
     let botImg = document.createElement("img");
     let botText = document.createElement("span");
     botDiv.id = "bot";
-    botImg.src = "bot-mini.png";
+    botImg.src = this.props.botIcon;
     botImg.className = "avatar";
     botDiv.className = "bot response";
-    botText.innerText = "Escribiendo...";
+    botText.innerText = "...";
     botDiv.appendChild(botText);
     botDiv.appendChild(botImg);
     messagesContainer.appendChild(botDiv);

e7032c45ce187d0b20e968906b9dda1f15d7e191
InigoRomero <igoromero@gmail.com>
2021-02-13 16:27:26 +0100

    add Icons attributes

--- example/src/App.js
+++ example/src/App.js
@@ -4,10 +4,18 @@ import ReactBot from 'react-chat-bot42'
 import 'react-chat-bot42/dist/index.css'
 
 const App = () => {
-  return <ReactBot  
-      userIcon={"user.png"}
-      botIcon={"bot.png"}
-  />
+  return (
+      <center>
+        <ReactBot  
+          userIcon={"user.png"}
+          botIcon={"bot.png"}
+        />
+        <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+      </center>
+  )
+  
 }
 
 export default App
